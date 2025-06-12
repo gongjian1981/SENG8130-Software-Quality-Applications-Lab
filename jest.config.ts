c357d07bf64e376687cb110b3fd4583f595af2ab
Jian Gong <gongjian1981@gmail.com>
2025-06-12 01:44:47 -0400

    test: configure Jest with ts-jest and jsdom

---
+++ jest.config.ts
@@ -0,0 +1,12 @@
+import { Config } from 'jest';
+
+const config: Config = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+};
+
+export default config;
